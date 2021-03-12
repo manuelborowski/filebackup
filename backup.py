4b86f8cd489400fb56176b44f3b1dcedca9ff147
manuel borowski <emmanuel.borowski@gmail.com>
2021-03-12 08:48:59 +0100

    V0.2: copy to the same directory structure as the original

--- backup.py
+++ backup.py
@@ -1,11 +1,12 @@
 # crontab -e
 # 0 */1 * * * cd /home/aboro/projects/sqlbackup && python backup.py >> backup_projects.log 2>&1
 
-# V0.1 : copy from sqlbackup
+# V0.1: copy from sqlbackup
+# V0.2: copy to the same directory structure as the original
 
 import sys, datetime, subprocess, json, argparse
 
-version = 'V0.1'
+version = 'V0.2'
 parser = argparse.ArgumentParser(description='store directories and files in cloud')
 parser.add_argument('--no-cloud', dest='store_in_cloud', action='store_false')
 parser.add_argument('--version', action='version', version=f'version: {version}')
@@ -28,7 +29,7 @@ try:
     for item in items:
         print(f'{timestamp}: >>> Backing up : {item} <<<<')
 
-        rclone = subprocess.run(f'rclone copy {item} {backup_path}'.split())
+        rclone = subprocess.run(f'rclone copy {item} {backup_path}/{item}'.split())
         if rclone.returncode != 0:
             print(f'{timestamp}: Error, could not rclone {item}')
             continue
